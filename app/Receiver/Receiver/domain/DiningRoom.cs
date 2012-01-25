07f598da981a7d2ed8d441a35f7398cca1dafd36
Sergio de la Rubia Garc?a-Carpintero <srug86@gmail.com>
2012-01-25 17:03:05 +0100

    - a?adidas los principales objetos del proyecto en Domain

---
+++ app/Receiver/Receiver/domain/DiningRoom.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Receiver.domain
+{
+    class DiningRoom
+    {
+        private List<Table> tables;
+
+        internal List<Table> Tables
+        {
+            get { return tables; }
+            set { tables = value; }
+        }
+        
+        public DiningRoom()
+        {
+        }
+    }
+}
